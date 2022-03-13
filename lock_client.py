c55af27f8f29fa9501a9699f17c7df92eacf36ef
Lucas Pinto <30878914+luckspt@users.noreply.github.com>
2022-03-13 14:26:51 -0700

    Update lock_client.py

--- lock_client.py
+++ lock_client.py
@@ -4,6 +4,7 @@
 Aplicações Distribuídas - Projeto 1 - lock_client.py
 Grupo: 21
 Números de aluno: 56895, 56926
+Nomes de aluno: Matilde Silva, Lucas Pinto
 """
 # Zona para fazer imports
 from argparse import ArgumentParser
